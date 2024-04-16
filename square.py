e27f56e9c0c52fe6a80d2cde41b40ad78f3185dc
merlin254-X <merlinderick8@gmail.com>
2024-04-16 21:25:37 +0000

    all

--- square.py
+++ square.py
@@ -1,28 +1,39 @@
 #!/usr/bin/python3
 
-class square():
-    
-    width = 0
-    height = 0
 
-    
-    def __init__(self, *args, **kwargs):
+class Square():
+  """
+  Represents a square with width and height properties.
+  """
+
+  width = 0
+  height = 0
+
+  def __init__(self, *args, **kwargs):
         for key, value in kwargs.items():
-            setattr(self, key, value)
+          setattr(self, key, value)
 
-    def area_of_my_square(self):
-        """ Area of the square """
-        return self.width * self.width
+  def area_of_my_square(self):
+    """
+    Calculates the area of the square.
+    """
+    return self.width * self.width
 
-    def PermiterOfMySquare(self):
-        return (self.width * 2) + (self.height * 2)
+  def perimeter_of_my_square(self):
+    """
+    Calculates the perimeter of the square.
+    """
+    return self.width * 4
 
-    def __str__(self):
-        return "{}/{}".format(self.width, self.height)
-
+  def __str__(self):
+    """
+    Returns a string representation of the square with width and height.
+    """
+    return "Square(width: %d, height: %d)" % (self.width, self.height)
 if __name__ == "__main__":
 
-    s = square(width=12, height=9)
-    print(s)
-    print(s.area_of_my_square())
-    print(s.PermiterOfMySquare())
+  s = Square(width=12, height=9)
+  print(s)
+  print(s.area_of_my_square())
+  print(s.perimeter_of_my_square())
+
--- user.py
+++ user.py
@@ -4,27 +4,35 @@ User class
 """
 
 class User():
-    """ Documentation """
+    """A class representing a user."""
 
     def __init__(self):
-        """ Documentation """
+        """Initialize a new User instance."""
         self.__email = None
-
-    @email.setter
-    def email(self, value):
-        """ Documentation """
-        if type(value) is not str:
-            raise TypeError("email must be a string")
-        self.__email = value
 
     @property
     def email(self):
-        """ Documentation """
+        """Get the email address of the user."""
         return self.__email
+
+    @email.setter
+    def email(self, value):
+        """
+        Set the email address of the user.
+
+        Args:
+            value (str): The email address to set.
+
+        Raises:
+            TypeError: If the input value is not a string.
+        """
+        if type(value) is not str:
+            raise TypeError("email must be a string")
+        self.__email = value
    
     
 if __name__ == "__main__":
-
     u = User()
     u.email = "john@snow.com"
     print(u.email)
+
